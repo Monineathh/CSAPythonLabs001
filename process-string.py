dbd842b3a67d52915c73d332c0b3d9df9e802275
Monineathh <143490227+Monineathh@users.noreply.github.com>
2024-01-28 16:07:01 +0700

    Sokchanmonineath

--- process-string.py
+++ process-string.py
@@ -3,8 +3,22 @@
 
 # Examples (Input -> Output):
 # * "String"      -> "SSttrriinngg"
+a = "String"
+for x in a:
+    result = ''.join(x * 2)
+    print(result, end = "")
+
 # * "Hello World" -> "HHeelllloo  WWoorrlldd"
+b = "Hello World"
+for y in b:
+    result = ''.join(y * 2)
+    print(result, end = "")
+
 # * "1234!_ "     -> "11223344!!__  "
+c = "1234!_"
+for z in c:
+    result = ''.join(z * 2)
+    print(result, end = "")
 
 
 # Application 2
@@ -18,7 +32,19 @@
 # Notes A hyphen will separate the two letters in the string.
 
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-start, end = alphabet.split('-')
-user_range = input("Enter a range of letters (e.g., a-z): ")
 
+# write down all of the alphabet total range
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ"
+# ask user to input letter from one - another using split
+user_input = input('Enter a range of letters (e.g., a-z): ')
+# analyze start and end point by getting the user input range e.g. a-m so, start = a
+start, end = user_input.split("-")
+# using index function to find the position of the start point e.g. a = 0
+start_num = alphabet.index(start)
+# using index function to find the position of the end point e.g. m = 13
+end_num = alphabet.index(end)
+# using range alphabet[num1:num2] which means the position of num1 to num2
+range = alphabet [start_num: end_num + 1]
+# e.g. alphabet[0:13] which means in alphabet variable, it displays from letter in 0 position to 13 position
+# note: variable[:] only works with number, not string and that's why we have to find the number of position in the string
+print(range)
